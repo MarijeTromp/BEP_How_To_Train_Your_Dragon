e54f1912fcc92893501641dd0a9443e681332a53
Bo Hofstede <q.b.hofstede-1@student.tudelft.nl>
2022-05-20 12:38:47 +0200

    Add grid search for parameters

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import itertools
 from typing import List
 from evaluation.experiment_procedure import *
 from example_parser.string_parser import StringParser
@@ -12,35 +13,48 @@ from search.batch_run import BatchRun
 from search.vlns.large_neighborhood_search.algorithms.remove_n_insert_n import RemoveNInsertN
 
 if __name__ == "__main__":
-    searchAlgos : List[Type[SearchAlgorithm]    ] = [
-        [VanillaGPReworked, "gpr"]
-    ]
+    # Possible Search Algorithms: Brute, MCTS, MetropolisHastings, LNS, VanillaGP
+    searchAlgos: List[Type[SearchAlgorithm]] = [[MetropolisHasting, "metro"]]
 
-    for domain in ["robot", "pixel", "string"]:
+    # Possible domains: "robot", "pixel", "string"
+    domains = ["robot"]
+
+    # For gridsearch on algorithm parameters
+    param_values = [
+        [10, 20, 50],  # add_token
+        [10, 20, 50],  # remove_token
+        [10, 20, 50],  # add_loop
+        [10, 20, 50],  # add_if_statement
+        [2, 10, 20]    # start_over
+    ]
+    param_grid_space = list(map(list, list(itertools.product(*param_values))))
+
+    for domain in domains:
         results = []
         for alg in searchAlgos:
-            result = BatchRun(
-            # Task domain
-            domain=domain,
+            for params in param_grid_space:
+                result = BatchRun(
+                    # Task domain
+                    domain=domain,
 
-            # Iterables for files name. Use [] to use all values.
-            # This runs all files adhering to format "2-*-[0 -> 10]"
-            # Thus, ([], [], []) runs all files for a domain.
-            files=([], [], []),
+                    # Iterables for files name. Use [] to use all values.
+                    # This runs all files adhering to format "2-*-[0 -> 10]"
+                    # Thus, ([], [], []) runs all files for a domain.
+                    files=([], [], []),
 
-            # Search algorithm to be used
-            search_algorithm=alg[0](60),
+                    # Search algorithm to be used
+                    search_algorithm=alg[0](60, params),
 
-            # Prints out result when a test case is finished
-            print_results=True,
+                    # Prints out result when a test case is finished
+                    print_results=True,
 
-            # Use multi core processing
-            multi_core=True,
+                    # Use multi core processing
+                    multi_core=True,
 
-            # Use file_name= to append to a file whenever a run got terminated
-            # Comment out argument to create new file.
-            #file_name="VLNS-20211213-162128.txt"
-            ).run()
+                    # Use file_name= to append to a file whenever a run got terminated
+                    # Comment out argument to create new file.
+                    # file_name="VLNS-20211213-162128.txt"
+                ).run()
 
         for res in results:
             print(res[0], res[1])
