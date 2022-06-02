f6124575fcf323493c0db046ca6f96fd4967e78a
Bo Hofstede <q.b.hofstede-1@student.tudelft.nl>
2022-06-02 17:28:03 +0200

    Add locality options for loops and if statements + default parameters

--- search/metropolis_hastings/metropolis.py
+++ search/metropolis_hastings/metropolis.py
@@ -22,21 +22,23 @@ class Mutation():
 
 class MetropolisHasting(SearchAlgorithm):
     def __init__(self, time_limit_sec: float, params: dict = {}):
-        mutation_params: dict = params
 
-        # Add default values if not in specified in params
-        if 'alpha' not in mutation_params.keys():
-            params['alpha'] = 1
-        if 'add_token' not in mutation_params.keys():
-            params['add_token'] = 10
-        if 'remove_token' not in mutation_params.keys():
-            params['remove_token'] = 20
-        if 'add_loop' not in mutation_params.keys():
-            params['add_loop'] = 10
-        if 'add_if_statement' not in mutation_params.keys():
-            params['add_if_statement'] = 10
-        if 'start_over' not in mutation_params.keys():
-            params['start_over'] = 2
+        # Default parameters
+        mutation_params: dict = {
+            "alpha": 1,
+            "add_token_end": 0,
+            "add_token_random": 0,
+            "remove_token_end": 0,
+            "remove_token_random": 0,
+            "add_loop_end": 0,
+            "add_loop_random": 0,
+            "add_if_statement_end": 0,
+            "add_if_statement_random": 0,
+            "start_over": 0
+        }
+
+        for k,v in params.items():
+            mutation_params[k] = v
 
         super().__init__(time_limit_sec, mutation_params)
 
@@ -47,20 +49,27 @@ class MetropolisHasting(SearchAlgorithm):
         self._best_program: Program = Program([])
         self.cost = 100
         self.proposal_distribution = ProposalDistribution()
+        # Find a way to systematically explore the types of mutations and their values,
+        # e.g. remove_random_token vs remove_last_token
         fac = MutationFactory()
-        self.proposal_distribution.add_mutation(fac.add_random_token(trans_tokens), self.params['add_token'])
-        self.proposal_distribution.add_mutation(fac.remove_random_token(), self.params['remove_token'])
-        self.proposal_distribution.add_mutation(fac.add_loop(bool_tokens, trans_tokens), self.params['add_loop'])
-        self.proposal_distribution.add_mutation(fac.add_if_statement(bool_tokens, trans_tokens), self.params['add_if_statement'])
-        self.proposal_distribution.add_mutation(fac.start_over(), self.params['start_over'])
+        self.proposal_distribution.add_mutation(fac.add_token_end(trans_tokens), self.params['add_token_end'])
+        self.proposal_distribution.add_mutation(fac.add_token_random(trans_tokens), self.params['add_token_random'])
 
+        self.proposal_distribution.add_mutation(fac.remove_token_end(), self.params['remove_token_end'])
+        self.proposal_distribution.add_mutation(fac.remove_token_random(), self.params['remove_token_random'])
+
+        self.proposal_distribution.add_mutation(fac.add_loop_end(bool_tokens, trans_tokens), self.params['add_loop_end'])
+        self.proposal_distribution.add_mutation(fac.add_if_statement_end(bool_tokens, trans_tokens), self.params['add_if_statement_end'])
+        self.proposal_distribution.add_mutation(fac.start_over(), self.params['start_over'])
 
     def iteration(self, examples: List[Example], trans_tokens, bool_tokens) -> bool:
         self.number_of_iterations += 1
         self.number_of_explored_programs += 1
         mut: Mutation = self.proposal_distribution.sample()
+        forward_transition, backward_transition = self.calc_transition_probabilities(mut)
+
         self._best_program, newcost, solved = MetropolisHasting.maybe_apply_mutation(
-            examples, self._best_program, self.cost, mut, self.params['alpha'])
+            examples, self._best_program, self.cost, mut, forward_transition, backward_transition, self.params['alpha'], self.params['type'])
 
         if (newcost != self.cost):
             self.cost_per_iteration.append((self.number_of_iterations, newcost))
@@ -71,9 +80,34 @@ class MetropolisHasting(SearchAlgorithm):
     def extend_result(self, search_result: SearchResult):
         return super().extend_result(search_result)
 
+    def calc_transition_probabilities(self, mut: Mutation):
+        total = self.proposal_distribution.get_total_weight()
+        forward_transition, backward_transition = 0, 0
+
+        # Examine math to find proper forward and backward probabilities for random position mutations
+        # Current approach: just use metropolis (assume forward == backward)
+        if mut.name == "add_token_end":
+            pro = self.params['add_token_end'], self.params['remove_token_end']
+        elif mut.name == "add_token_random":
+            pro = self.params['add_token_random'], self.params['remove_token_random']
+        elif mut.name == "remove_token_end":
+            pro = self.params['remove_token_end']
+        elif mut.name == "remove_token_random":
+            return 1, 1
+        elif mut.name == "add_loop_end":
+            pro = self.params['add_loop_end']
+        elif mut.name == "add_if_statement_end":
+            pro = self.params['add_if_statement_end']
+        elif mut.name == "start_over":
+            # Explore alternatives for restarts
+            return 1, 1
+
+        return forward_transition / total, backward_transition / total
+
+
     @staticmethod
-    def maybe_apply_mutation(examples: List[Example], old_program: Program, ocost: int, mut: Mutation, alpha: float) -> Tuple[
-        Program, int, int]:
+    def maybe_apply_mutation(examples: List[Example], old_program: Program, ocost: int, mut: Mutation,
+                             forward_transition, backward_transition, alpha: float, type: str) -> Tuple[Program, int, int]:
         new_program = mut.apply(old_program)
         try:
             cost = 0
@@ -90,7 +124,11 @@ class MetropolisHasting(SearchAlgorithm):
                     nenv = new_program.interp(case.input_environment)
                     solved = solved and nenv.correct(case.output_environment)
 
+            # Default is Metropolis
             ratio = math.exp(-alpha * cost) / math.exp(-alpha * ocost)
+            if type == "metropolis_hastings":
+                ratio *= forward_transition / backward_transition
+
             if ratio > 1:
                 return new_program, cost, solved
             if random.random() < ratio:
@@ -99,6 +137,8 @@ class MetropolisHasting(SearchAlgorithm):
 
         except(InvalidTransition, LoopIterationLimitReached):
             return old_program, ocost, False
+
+
 
 
 class ProposalDistribution():
@@ -120,28 +160,45 @@ class ProposalDistribution():
                 return mut
             choice -= pro
 
+    def get_total_weight(self):
+        return sum([mut[1] for mut in self.mutations])
 
 # The operation must never be allowed to modify the Program that is passed in!
 class MutationFactory():
     def __init__(self):
         pass
 
-    def add_random_token(self, trans_tokens) -> Mutation:
+    # Adds a random token to the end of the program
+    def add_token_end(self, trans_tokens) -> Mutation:
         def operation(pro: Program) -> Program:
             rand_token = random.choice(list(trans_tokens))
             return Program(pro.sequence + [rand_token])
 
-        return Mutation("add_random_token", operation)
+        return Mutation("add_token_end", operation)
 
-
-    def remove_last_token(self) -> Mutation:
+    # Adds a random token in a random position in the program
+    def add_token_random(self, trans_tokens) -> Mutation:
         def operation(pro: Program) -> Program:
-            return Program(pro.sequence[:len(pro.sequence)-1])
+            rand_token = random.choice(list(trans_tokens))
 
-        return Mutation("remove_last_token", operation)
+            length = len(pro.sequence)
+            if length == 0:
+                return Program([rand_token])
+            idk = random.randrange(length + 1)
 
+            return Program(pro.sequence[:idk] + [rand_token] + pro.sequence[idk:])
 
-    def remove_random_token(self) -> Mutation:
+        return Mutation("add_token_random", operation)
+
+    # Removes the last token in the program
+    def remove_token_end(self) -> Mutation:
+        def operation(pro: Program) -> Program:
+            return Program(pro.sequence[:len(pro.sequence) - 1])
+
+        return Mutation("remove_token_end", operation)
+
+    # Removes a random token from the program
+    def remove_token_random(self) -> Mutation:
         def operation(pro: Program) -> Program:
             length = len(pro.sequence)
             if length == 0:
@@ -149,24 +206,57 @@ class MutationFactory():
             idk = random.randrange(length)
             return Program(pro.sequence[:idk] + pro.sequence[idk + 1:])
 
-        return Mutation("remove_random_token", operation)
+        return Mutation("remove_token_random", operation)
 
-    def add_loop(self, bool_tokens, trans_tokens) -> Mutation:
+
+    def add_loop_end(self, bool_tokens, trans_tokens) -> Mutation:
         def operation(pro: Program) -> Program:
             rand_bool = random.choice(list(bool_tokens))
             rand_token = random.choice(list(trans_tokens))
             return Program(pro.sequence + [LoopWhile(rand_bool, [rand_token])])
 
-        return Mutation("add_loop", operation)
+        return Mutation("add_loop_end", operation)
 
-    def add_if_statement(self, bool_tokens, trans_tokens) -> Mutation:
+    def add_loop_random(self, bool_tokens, trans_tokens) -> Mutation:
+        def operation(pro: Program) -> Program:
+            rand_bool = random.choice(list(bool_tokens))
+            rand_token = random.choice(list(trans_tokens))
+
+            length = len(pro.sequence)
+            if length == 0:
+                return Program([LoopWhile(rand_bool, [rand_token])])
+            idk = random.randrange(length + 1)
+
+            return Program(pro.sequence[:idk] + [LoopWhile(rand_bool, [rand_token])] + pro.sequence[idk:])
+
+        return Mutation("add_loop_random", operation)
+
+
+    def add_if_statement_end(self, bool_tokens, trans_tokens) -> Mutation:
         def operation(pro: Program) -> Program:
             rand_bool = random.choice(list(bool_tokens))
             rand_token = random.choice(list(trans_tokens))
             rand_token2 = random.choice(list(trans_tokens))
             return Program(pro.sequence + [If(rand_bool, [rand_token], [rand_token2])])
 
-        return Mutation("add_if_statement", operation)
+        return Mutation("add_if_statement_end", operation)
+
+
+    def add_if_statement_random(self, bool_tokens, trans_tokens) -> Mutation:
+        def operation(pro: Program) -> Program:
+            rand_bool = random.choice(list(bool_tokens))
+            rand_token = random.choice(list(trans_tokens))
+            rand_token2 = random.choice(list(trans_tokens))
+
+            length = len(pro.sequence)
+            if length == 0:
+                return Program([If(rand_bool, [rand_token], [rand_token2])])
+            idk = random.randrange(length + 1)
+
+            return Program(pro.sequence[:idk] + [If(rand_bool, [rand_token], [rand_token2])] + pro.sequence[idk:])
+
+        return Mutation("add_if_statement_random", operation)
+
 
     def start_over(self) -> Mutation:
         def operation(_: Program) -> Program:
