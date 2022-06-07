0b7c1c65961d8e7c0aa75a4b1d7572b0abffd62c
Rixt Hellinga <rhellinga@student.tudelft.nl>
2022-06-07 21:23:19 +0200

    Added pseudocode to equivalence_matching.py for different ideas of pruning

--- search/vlns/large_neighborhood_search/prune/equivalence_matching.py
+++ search/vlns/large_neighborhood_search/prune/equivalence_matching.py
@@ -7,4 +7,62 @@ If k < limit then we create a new equivalence class
 
 BUT - this way equivalence classes will not be updated :/
 AND - as the algorithm progresses, we have more and more classes, meaning fewer and fewer programs will be explored
+
+
+observational equivalence pruning is mostly useful when it is not possible to reach a better state from that state
+so if we apply x steps to the state, and it remains equal, then we can put the state in a "bad-list"
+we use that "bad-list" for observational equivalence pruning.
+Any time a state like that appears in the neighborhood, we ignore it
+(and do not count is as part of the explore neighborhood, but that might cause empty neighborhoods and deadlocks)
+(empty neighborhoods aren't the worst thing though, as there are iterations before we increase search depth)
+(so deadlocks are actually unlikely (because of randomness the neighborhood changes each time))
+
+
+simple implementation:
+(IDEA: only prune on observational equivalence between direct neighbors)
+(NOTE: this would only have effect on situations in which no improvement is found,
+in which case the observationally equivalent neighbor would generally be chosen)
+explore neighborhood:
+- i times
+- - get neighbor
+- - get neighbor's output state
+- - if neighbor's cost < best neighbor's cost
+- - - if output state of best neighbor !equals that of current solution
+- - - - best neighbor = neighbor
+- - - else (observationally equivalent) if *somechance*
+- - - - best neighbor = neighbor
+- ...continue with algorithm...
+
+dead-end implementation: (USELESS IMPLEMENTATION)
+(problem is that pruning on observational equivalence on dead states is
+that using a counter to bring in stochasticity is useless, as any dead state
+should not be explored)
+(NOTE: this won't work at all, because a dead end in the VLNS algorithm is a general dead end.
+VLNS has no backtracking... So this would go wrong at the very first dead end
+(which is unlikely to even occur at all, making the technique useless))
+
+explore neighborhood:
+- i times
+- - get neighbor
+- - get neighbor's output state
+- - if output state is in dead-list
+- - - continue (skips one iteration of the loop)
+- - if neighbor's cost < best neighbor's cost
+- - - best neighbor = neighbor
+- ...continue with algorithm...
+
+applying x steps:
+- save the old program
+- do x times:
+- - program = repair(destroy(program))
+- if program = old program (observational equivalence)
+- then we have dead end
+- add program's output state to bad-list
+- increment counter of that entry in dead-list
+
+dead-list:
+- hashmap
+- keys: example world
+- values: dead states
+
 """
